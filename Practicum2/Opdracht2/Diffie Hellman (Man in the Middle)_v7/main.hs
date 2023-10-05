1b79dda0ddc43fe61dd147ca50330e6cf47a8c01
namelesshacker <gally_gb@hotmail.com>
2023-10-05 17:51:00 +0200

    update practicum 2

---
+++ Practicum2/Opdracht2/Diffie Hellman (Man in the Middle)_v7/main.hs
@@ -0,0 +1,20 @@
+
+module Main (main) where
+
+import MIMA
+
+--rest = print $ (euclid 2 3)
+main :: IO ()
+main = print $ (euclid 36 15)
+
+--C:\Users\gally\Documents\FuncProg\Practicum2\Opdracht1a
+--fact n = if n == 0 then 1 else n * fact(n-1)
+--main = print (fact 5)
+
+
+ 
+
+-- ghc --make -XQuasiQuotes LibTester.hs -o LibTester
+--ghc --make -XQuasiQuotes main.hs -o main
+
+
